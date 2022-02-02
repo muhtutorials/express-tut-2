ab64ea0cdc2848c5d8dd488f472efeb0c95f03d6
igor <igorwho@yandex.ru>
2022-02-02 16:20:29 +0300

    Four

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,20 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
 module.exports = {
-  hello() {
-    return {
-      text: 'hey!',
-      value: 10
-    };
+  createUser: async function(args, req) {
+    const { email, name, password } = args.userInput;
+    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error('User already exists.');
+      throw error;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({ email, password: hashedPassword, name });
+
+    return { ...user._doc, _id: user._id.toString() };
   }
 }
